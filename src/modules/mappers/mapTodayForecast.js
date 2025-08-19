0e4ee87aaba8363f0968994cb9dc27f53a704fc8
Daria <kordesovad@gmail.com>
2025-08-19 18:21:22 +0200

    refactoring: refactor mapTodayForecast.js

--- src/modules/mappers/mapTodayForecast.js
+++ src/modules/mappers/mapTodayForecast.js
@@ -1,14 +1,14 @@
 export function mapTodayForecast (weatherData) {
-    let todayForecast = {};
-	todayForecast.city = weatherData.location.name;
-	todayForecast.country = weatherData.location.country;
-	todayForecast.localtime = weatherData.location.localtime;
-	todayForecast.condition = weatherData.current.condition.text;
-	todayForecast.humidity = weatherData.current.humidity;
-	todayForecast.tempC = weatherData.current.temp_c;
-	todayForecast.tempF = weatherData.current.temp_f;
-	todayForecast.windKPH = weatherData.current.wind_kph;
-	todayForecast.cloud = weatherData.current.cloud;
-	todayForecast.icon = weatherData.current.condition.icon;
+    let todayForecast = {
+		city: weatherData.location.name,
+		localtime: weatherData.location.localtime,
+		condition: weatherData.current.condition.text,
+		humidity: weatherData.current.humidity,
+		tempC: weatherData.current.temp_c,
+		tempF: weatherData.current.temp_f,
+		windKPH: weatherData.current.wind_kph,
+		cloud: weatherData.current.cloud,
+		icon: weatherData.current.condition.icon,
+	};
 	return todayForecast;
 }
