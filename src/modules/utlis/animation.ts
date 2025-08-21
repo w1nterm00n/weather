fc7e1514f1c09282d9eb252494b7856ac10dfea0
Daria <kordesovad@gmail.com>
2025-08-21 19:59:16 +0200

    installed ts, switched file extensions to .ts

---
+++ src/modules/utlis/animation.ts
@@ -0,0 +1,17 @@
+export async function playAnim(el, animation, delayMs = 0) {
+    return new Promise(resolve => {
+      el.style.setProperty('--animate-delay', `${delayMs}ms`);
+      el.classList.add('animate__animated', animation);
+
+      function onEnd(e) {
+        e.stopPropagation();
+        el.classList.remove('animate__animated', animation);
+        el.style.removeProperty('--animate-delay');
+        el.style.opacity = '1';
+        el.removeEventListener('animationend', onEnd);
+        resolve();
+      }
+      el.addEventListener('animationend', onEnd, { once: true });
+    });
+  }
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	mode: 'development',
-	context: path.resolve(__dirname, "src"),
+	context: path.resolve(__dirname, "build"),
 	entry: "./modules/script.js",
 	devtool: "source-map",
 	output: {
