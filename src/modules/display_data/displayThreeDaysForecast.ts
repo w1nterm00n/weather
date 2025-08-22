209f5ab2f4cae8c32b9795e09e998a5650648ffe
Daria <kordesovad@gmail.com>
2025-08-22 10:14:32 +0200

    typiy displayThreeDaysForecast.ts

--- src/modules/display_data/displayThreeDaysForecast.ts
+++ src/modules/display_data/displayThreeDaysForecast.ts
@@ -1,9 +1,9 @@
 export function displayThreeDayForecast (threeDayForecastArr, scale) {
 	//displaying forecast for 3 days
-	let futureForecast = document.querySelector(".futureForecastWrapper");
+	let futureForecast = document.querySelector(".futureForecastWrapper") as HTMLElement;
 	futureForecast.style.display = "flex";
 
-	let getForecastIcon = async function (iconLink, dayIcon) {
+	let getForecastIcon = async function (iconLink: string, dayIcon: HTMLElement) {
 		try {
 			let searchString = "https:" + iconLink;
 			let response = await fetch(searchString, { mode: "cors" });
@@ -15,19 +15,19 @@ export function displayThreeDayForecast (threeDayForecastArr, scale) {
 		}
 	};
 
-	function displayDayNForecast (n) {
-		let dayN = futureForecast.querySelector(`.day${n}Wrapper`);
-		let dateN = dayN.querySelector(".date");
+	function displayDayNForecast (n: number) {
+		let dayN = futureForecast.querySelector(`.day${n}Wrapper`) as HTMLElement;
+		let dateN = dayN.querySelector(".date") as HTMLElement;
 		dateN.textContent = threeDayForecastArr[n].date;
-		let dayTN = dayN.querySelector(`#day${n}temp`);
-		let nightTN = dayN.querySelector(`#night${n}temp`);
+		let dayTN = dayN.querySelector(`#day${n}temp`) as HTMLElement;
+		let nightTN = dayN.querySelector(`#night${n}temp`) as HTMLElement;
 
 		const dayKey = scale === "C" ? "dayTC" : "dayTF";
 		const nightKey = scale === "C" ? "nightTC" : "nightTF";
 		dayTN.textContent = `${threeDayForecastArr[n][dayKey]} °${scale}`;
 		nightTN.textContent = `${threeDayForecastArr[n][nightKey]} °${scale}`;
 
-		let dayNIcon = dayN.querySelector(".icon");
+		let dayNIcon = dayN.querySelector(".icon") as HTMLElement;
 		let iconNLink = threeDayForecastArr[n].icon;
 		getForecastIcon(iconNLink, dayNIcon);
 	}
